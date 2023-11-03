34432e6f577e2fbb5eb395bc7a164e6e3252aeaf
Arshad Khan <khanarshad0003@outlook.com>
2023-11-04 01:00:41 +0530

    Create FIFO.java

---
+++ SPOS/Practical 5/FIFO.java
@@ -0,0 +1,43 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class FIFO {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of frames: ");
+        int numFrames = scanner.nextInt();
+
+        System.out.print("Enter the sequence of page references (e.g., 1 2 3 4 1 2): ");
+        String input = scanner.nextLine();
+        String[] inputPages = scanner.nextLine().split(" ");
+
+        Queue<Integer> frameQueue = new ArrayDeque<>(numFrames);
+        int pageFaults = 0;
+
+        System.out.println("\nPage Replacement Steps:");
+
+        for (String pageStr : inputPages) {
+            int page = Integer.parseInt(pageStr);
+
+            if (!frameQueue.contains(page)) {
+                if (frameQueue.size() == numFrames) {
+                    int removedPage = frameQueue.poll();
+                    System.out.println("Page " + removedPage + " removed from the frame.");
+                }
+
+                frameQueue.offer(page);
+                pageFaults++;
+                System.out.println("Page " + page + " added to the frame.");
+            } else {
+                System.out.println("Page " + page + " already in the frame.");
+            }
+        }
+        scannet.close();
+        System.out.println("\nTotal page faults: " + pageFaults);
+        System.out.println();
+        System.out.println();
+        System.out.println("By Arshad Khan : @whoami0003.py");
+    }
+}
