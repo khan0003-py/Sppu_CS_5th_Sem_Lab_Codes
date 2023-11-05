a12a1058e41771c68b333fb8a3c223c91535d8c9
Arshad Khan <khanarshad0003@outlook.com>
2023-11-05 21:22:59 +0530

    Added Priority

--- SPOS/Practical 4/Priority.java
+++ SPOS/Practical 4/Priority.java
@@ -7,15 +7,17 @@ class Process {
     int completionTime;
     int turnAroundTime;
     int waitingTime;
+    int priority;
 
-    Process(String processName, int arrivalTime, int burstTime) {
+    Process(String processName, int arrivalTime, int burstTime, int priority) {
         this.processName = processName;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
+        this.priority = priority;
     }
 }
 
-public class SJF {
+public class Priority  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, awt = 0, atat = 0;
@@ -33,28 +35,28 @@ public class SJF {
             int arrivalTime = sc.nextInt();
             System.out.println("Enter Burst Time of Process " + (i + 1) + ": ");
             int burstTime = sc.nextInt();
-            processes[i] = new Process("P" + (i + 1), arrivalTime, burstTime);
+            System.out.println("Enter Priority of Process " + (i + 1) + ": ");
+            int priority = sc.nextInt();
+            processes[i] = new Process("P" + (i + 1), arrivalTime, burstTime, priority);
         }
 
         int currentTime = 0;
         boolean allCompleted = false;
 
         while (!allCompleted) {
-            int minBurstTime = Integer.MAX_VALUE;
+            int highestPriority = Integer.MIN_VALUE;
             int selectedProcess = -1;
 
             for (int i = 0; i < n; i++) {
-                if (!b[i] && processes[i].arrivalTime <= currentTime && processes[i].burstTime < minBurstTime) {
-                    minBurstTime = processes[i].burstTime;
+                if (!b[i] && processes[i].arrivalTime <= currentTime && processes[i].priority > highestPriority) {
+                    highestPriority = processes[i].priority;
                     selectedProcess = i;
                 }
             }
 
             if (selectedProcess == -1) {
-                // No process is available, move time forward
                 currentTime++;
             } else {
-                // Execute the selected process
                 b[selectedProcess] = true;
                 currentTime += processes[selectedProcess].burstTime;
                 processes[selectedProcess].completionTime = currentTime;
@@ -76,13 +78,14 @@ public class SJF {
         float averageWaitingTime = (float) awt / n;
         float averageTurnAroundTime = (float) atat / n;
 
-        System.out.println("Process\tAT \tBT \tCT \tTAT \tWT");
+        System.out.println("Process\tAT \tBT \tPT \tCT \tTAT \tWT");
         for (int i = 0; i < n; i++) {
             System.out.println(processes[i].processName + "\t" + processes[i].arrivalTime + "\t"
-                    + processes[i].burstTime + "\t" + processes[i].completionTime + "\t" + processes[i].turnAroundTime
-                    + "\t" + processes[i].waitingTime);
+                    + processes[i].burstTime + "\t" + processes[i].priority + "\t" + processes[i].completionTime + "\t"
+                    + processes[i].turnAroundTime + "\t" + processes[i].waitingTime);
         }
         sc.close();
+        System.out.println();
         System.out.println("Average waiting time : " + averageWaitingTime);
         System.out.println("Average turn around time : " + averageTurnAroundTime);
         System.out.println();
